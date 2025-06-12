94581b916d67e072aac9a11228c2de9e570a8d36
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-06-12 11:40:06 +0200

    Creating route for user authentification

---
+++ routes/user.routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("../middleware/auth.middleware");
+const { registerUser, loginUser } = require("../controllers/auth.controller");
+
+//Defining route for registration
+router.post("/register", registerUser);
+//Defining route for user Login
+router.post("/login", loginUser);
+
