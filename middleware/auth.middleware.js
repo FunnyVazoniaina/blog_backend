3147043dc645cbd631fa8a32067d0d1b9dc8d8ae
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-06-12 11:42:03 +0200

    Implementing middleware for user authentification in protected route

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+//middleware for token verification
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1]; // Extract token from Authorization header
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "No token provided, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // Attach user info to request object
+    next(); // Proceed to the next middleware or route handler
+  } catch (error) {
+    return res.status(401).json({ message: "Token is not valid" });
+  }
+};
+module.exports = authMiddleware;
