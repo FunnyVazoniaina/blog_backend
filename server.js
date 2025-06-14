238febab08dec6aa21a8140eb3d44d37286d8ef4
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-06-14 13:26:33 +0200

    Feat: Implementing CORS and helmet for App security

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/user.routes");
+const helmet = require("helmet");
+const cors = require("cors");
 require("dotenv").config();
 
 //Initialize the express application
 const app = express();
+
+// Middleware to set security headers
+app.use(helmet());
+
+// Middleware to enable CORS (Cross-Origin Resource Sharing)
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 // Middleware to parse URL-encoded bodies like form submissions
 app.use(express.urlencoded({ extended: true }));
+
 // Middleware to parse JSON bodies
 app.use(express.json());
+
 //Connect to MongoDB
 connectDB();
 
