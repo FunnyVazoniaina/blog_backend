a5c03ec183a429a8fb9f4d5d35a2bcb65ca07851
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-06-14 17:41:15 +0200

    Style: rename middleware to be more sementic

---
+++ middlewares/generateSlug.middleware.js
@@ -0,0 +1,12 @@
+const slugify = require("slugify");
+
+/**
+ * Middleware Mongoose pour générer automatiquement un slug à partir du titre d’un post.
+ */
+export const generateSlug = function (next) {
+  if (this.title && !this.slug) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+  next();
+};
+module.exports = generateSlug;
