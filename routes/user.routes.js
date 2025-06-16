96440374bfd4f54cce1c97725a0ec38fbe56520e
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-06-16 16:59:40 +0200

    Chore: Adding authMiddleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authMiddleware = require("../middleware/auth.middleware");
+const authMiddleware = require("../middlewares/auth.middleware");
 const { registerUser, loginUser } = require("../controllers/auth.controller");
 
 //Defining route for registration
