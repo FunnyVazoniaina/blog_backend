de054fd99ba97ee15ea442c38b5566b0e5ce6638
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-06-19 14:30:56 +0200

    Feat: Implementing delete functionality

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -5,6 +5,7 @@ const {
   getAllPosts,
   getPostBySlug,
   updatePostBySlug,
+  deletePostBySlug,
   getPostsByAuthor,
 } = require("../controllers/post.controller");
 
@@ -12,6 +13,7 @@ router.post("/create", createPost);
 router.get("/allPosts", getAllPosts);
 router.get("/:slug", getPostBySlug);
 router.put("/:slug", updatePostBySlug);
+router.delete("/:slug", deletePostBySlug);
 router.get("/author/:authorId", getPostsByAuthor);
 
 module.exports = router;
